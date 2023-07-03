55903c11616f2257fc2ff40128ecf462f575f3be
Alek Earlz (ash2q) <ash@ash2q.com>
2023-07-03 16:49:54 -0600

    Add description and stats to equip menu; various other changes

---
+++ src/7.loot.lua
@@ -0,0 +1,18 @@
+--loot management
+
+function gen_loot_item(stat_max)
+    local g=nil
+    g.slot=flr(rnd(7)+1)
+    e.patk=rnd(stat_max)
+	e.pspd=rnd(stat_max)
+	e.pdef=rnd(stat_max)
+	e.ablt=rnd(stat_max)
+	e.wspd=rnd(stat_max)
+end
+
+function gen_loot()
+
+end
+
+
+
